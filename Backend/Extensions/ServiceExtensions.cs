ad61183e5d7905fbd19396db696c3d73f4e481a3
RobertF <robert.fillingham@linkthinks.com>
2025-11-09 11:07:14 -0600

    Register COCOMO II Stage 3 services and repositories
    Added dependency injection registrations for COCOMO II Stage 3 services and repositories in ServiceExtensions.cs. Updated COCOMOIISTAGE3_REPLICATION_PLAN.txt to mark integration and configuration phase as completed.

--- Backend/Extensions/ServiceExtensions.cs
+++ Backend/Extensions/ServiceExtensions.cs
@@ -7,18 +7,22 @@ using Backend.Repositories.Implementations;
 using Backend.Repositories.Implementations.CocomoThree;
 using Backend.Repositories.Implementations.CocomoOne;
 using Backend.Repositories.Implementations.CocomoTwoStageOne;
+using Backend.Repositories.Implementations.CocomoIIStage3;
 using Backend.Repositories.Interfaces;
 using Backend.Repositories.Interfaces.CocomoThree;
 using Backend.Repositories.Interfaces.CocomoOne;
 using Backend.Repositories.Interfaces.CocomoTwoStageOne;
+using Backend.Repositories.Interfaces.CocomoIIStage3;
 using Backend.Services.Implementations;
 using Backend.Services.Implementations.CocomoThree;
 using Backend.Services.Implementations.CocomoOne;
 using Backend.Services.Implementations.CocomoTwoStageOne;
+using Backend.Services.Implementations.CocomoIIStage3;
 using Backend.Services.Interfaces;
 using Backend.Services.Interfaces.CocomoThree;
 using Backend.Services.Interfaces.CocomoOne;
 using Backend.Services.Interfaces.CocomoTwoStageOne;
+using Backend.Services.Interfaces.CocomoIIStage3;
 
 namespace Backend.Extensions;
 
@@ -33,13 +37,12 @@ public static class ServiceExtensions
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         // Register COCOMO 3 services
-        services.AddScoped<IAuthService, AuthService>();
-        services.AddScoped<IParameterSetService, ParameterSetService>();
-        services.AddScoped<ICocomoCalculationService, CocomoCalculationService>();
-        services.AddScoped<ILanguageService, LanguageService>();
-        services.AddScoped<IProjectService, ProjectService>();
-        services.AddScoped<IEstimationService, EstimationService>();
-        services.AddScoped<IEstimationFunctionService, EstimationFunctionService>();
+        services.AddScoped<Backend.Services.Interfaces.CocomoThree.IParameterSetService, Backend.Services.Implementations.CocomoThree.ParameterSetService>();
+        services.AddScoped<Backend.Services.Interfaces.CocomoThree.ICocomoCalculationService, Backend.Services.Implementations.CocomoThree.CocomoCalculationService>();
+        services.AddScoped<Backend.Services.Interfaces.CocomoThree.ILanguageService, Backend.Services.Implementations.CocomoThree.LanguageService>();
+        services.AddScoped<Backend.Services.Interfaces.CocomoThree.IProjectService, Backend.Services.Implementations.CocomoThree.ProjectService>();
+        services.AddScoped<Backend.Services.Interfaces.CocomoThree.IEstimationService, Backend.Services.Implementations.CocomoThree.EstimationService>();
+        services.AddScoped<Backend.Services.Interfaces.CocomoThree.IEstimationFunctionService, Backend.Services.Implementations.CocomoThree.EstimationFunctionService>();
         
         // Register COCOMO 1 services
         services.AddScoped<ICocomo1EstimationService, Cocomo1EstimationService>();
@@ -49,6 +52,14 @@ public static class ServiceExtensions
         services.AddScoped<IParameterSetCocomo2Stage1Service, ParameterSetCocomo2Stage1Service>();
         services.AddScoped<IEstimationCocomo2Stage1Service, EstimationCocomo2Stage1Service>();
         services.AddScoped<IComponentCocomo2Stage1Service, ComponentCocomo2Stage1Service>();
+        
+        // Register COCOMO II Stage 3 services
+        services.AddScoped<Backend.Services.Interfaces.CocomoIIStage3.IParameterSetService, Backend.Services.Implementations.CocomoIIStage3.ParameterSetService>();
+        services.AddScoped<Backend.Services.Interfaces.CocomoIIStage3.ICocomoCalculationService, Backend.Services.Implementations.CocomoIIStage3.CocomoCalculationService>();
+        services.AddScoped<Backend.Services.Interfaces.CocomoIIStage3.ILanguageService, Backend.Services.Implementations.CocomoIIStage3.LanguageService>();
+        services.AddScoped<Backend.Services.Interfaces.CocomoIIStage3.IProjectService, Backend.Services.Implementations.CocomoIIStage3.ProjectService>();
+        services.AddScoped<Backend.Services.Interfaces.CocomoIIStage3.IEstimationService, Backend.Services.Implementations.CocomoIIStage3.EstimationService>();
+        services.AddScoped<Backend.Services.Interfaces.CocomoIIStage3.IEstimationFunctionService, Backend.Services.Implementations.CocomoIIStage3.EstimationFunctionService>();
         
         // Register KLOC services
         services.AddScoped<IKlocEstimationService, KlocEstimationService>();
@@ -69,11 +80,11 @@ public static class ServiceExtensions
     {
         // Register COCOMO 3 repositories
         services.AddScoped<IUserRepository, UserRepository>();
-        services.AddScoped<IParameterSetRepository, ParameterSetRepository>();
-        services.AddScoped<ILanguageRepository, LanguageRepository>();
-        services.AddScoped<IProjectRepository, ProjectRepository>();
-        services.AddScoped<IEstimationRepository, EstimationRepository>();
-        services.AddScoped<IEstimationFunctionRepository, EstimationFunctionRepository>();
+        services.AddScoped<Backend.Repositories.Interfaces.CocomoThree.IParameterSetRepository, Backend.Repositories.Implementations.CocomoThree.ParameterSetRepository>();
+        services.AddScoped<Backend.Repositories.Interfaces.CocomoThree.ILanguageRepository, Backend.Repositories.Implementations.CocomoThree.LanguageRepository>();
+        services.AddScoped<Backend.Repositories.Interfaces.CocomoThree.IProjectRepository, Backend.Repositories.Implementations.CocomoThree.ProjectRepository>();
+        services.AddScoped<Backend.Repositories.Interfaces.CocomoThree.IEstimationRepository, Backend.Repositories.Implementations.CocomoThree.EstimationRepository>();
+        services.AddScoped<Backend.Repositories.Interfaces.CocomoThree.IEstimationFunctionRepository, Backend.Repositories.Implementations.CocomoThree.EstimationFunctionRepository>();
         
         // Register COCOMO 1 repositories
         services.AddScoped<ICocomo1EstimationRepository, Cocomo1EstimationRepository>();
@@ -82,6 +93,13 @@ public static class ServiceExtensions
         services.AddScoped<IParameterSetCocomo2Stage1Repository, ParameterSetCocomo2Stage1Repository>();
         services.AddScoped<IEstimationCocomo2Stage1Repository, EstimationCocomo2Stage1Repository>();
         services.AddScoped<IComponentCocomo2Stage1Repository, ComponentCocomo2Stage1Repository>();
+        
+        // Register COCOMO II Stage 3 repositories
+        services.AddScoped<Backend.Repositories.Interfaces.CocomoIIStage3.IParameterSetRepository, Backend.Repositories.Implementations.CocomoIIStage3.ParameterSetRepository>();
+        services.AddScoped<Backend.Repositories.Interfaces.CocomoIIStage3.ILanguageRepository, Backend.Repositories.Implementations.CocomoIIStage3.LanguageRepository>();
+        services.AddScoped<Backend.Repositories.Interfaces.CocomoIIStage3.IProjectRepository, Backend.Repositories.Implementations.CocomoIIStage3.ProjectRepository>();
+        services.AddScoped<Backend.Repositories.Interfaces.CocomoIIStage3.IEstimationRepository, Backend.Repositories.Implementations.CocomoIIStage3.EstimationRepository>();
+        services.AddScoped<Backend.Repositories.Interfaces.CocomoIIStage3.IEstimationFunctionRepository, Backend.Repositories.Implementations.CocomoIIStage3.EstimationFunctionRepository>();
         
         // Register KLOC repositories
         services.AddScoped<IKlocEstimationRepository, KlocEstimationRepository>();
